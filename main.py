0f3a55aced5b749d245b542959e1206e288fdf84
Jeralin <jeralinflores4@gmail.com>
2025-06-29 08:28:38 -0600

    Agregue funciones de update y delete

--- main.py
+++ main.py
@@ -53,3 +53,38 @@ def obtener_tareas():
     cursor.close()
     db.close()
     return tareas
+
+
+@app.put("/tasks/{task_id}")
+def actualizar_tarea(task_id: int, tarea: Tarea):
+    db = conectar_db()
+    cursor = db.cursor()
+    sql = """
+        UPDATE tasks
+        SET taskname = %s, taskdescription = %s, taskpriority = %s, taskdeadline = %s, status = %s
+        WHERE id = %s
+    """
+    valores = (
+        tarea.taskname,
+        tarea.taskdescription,
+        tarea.taskpriority,
+        tarea.taskdeadline,
+        tarea.status,
+        task_id
+    )
+    cursor.execute(sql, valores)
+    db.commit()
+    cursor.close()
+    db.close()
+    return {"mensaje": f"Tarea con ID {task_id} actualizada"}
+
+@app.delete("/tasks/{task_id}")
+def eliminar_tarea(task_id: int):
+    db = conectar_db()
+    cursor = db.cursor()
+    sql = "DELETE FROM tasks WHERE id = %s"
+    cursor.execute(sql, (task_id,))
+    db.commit()
+    cursor.close()
+    db.close()
+    return {"mensaje": f"Tarea con ID {task_id} eliminada"}
